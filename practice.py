0cceb2912d51719d55f1cd25a747ba2c1440c3ef
Dasha-K8 <karlova.dsh@gmail.com>
2025-10-01 12:28:57 +0300

    Initial commit

--- practice.py
+++ practice.py
@@ -9,7 +9,7 @@ payload = {"q": "компьютер"}
 response = requests.post(url, json=payload)
 data = response.json()
 
-with open("../autotests-api/practice1.csv", "w") as file:
+with open("../tasks/practice1.csv", "w") as file:
     writer = csv.DictWriter(file, fieldnames=data)
     writer.writerow(data)
 
