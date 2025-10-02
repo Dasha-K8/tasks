14c355e00e59d7eb15b180496a8d0b27a4d3e4c6
Dasha-K8 <karlova.dsh@gmail.com>
2025-10-02 11:12:07 +0300

    new commit

--- practice.py
+++ practice.py
@@ -1,27 +1,30 @@
 import requests
 import csv
 
-url = "https://api.tenderbot.kz/api/lots"
+base_url = "https://api.tenderbot.kz/api/lots?page="
+last_page = 15
 
 keyword = "компьютер"
 payload = {
     "search_tags": [{"text": keyword, "tiClasses": ["ti-valid"]}],
     "searchstr": keyword
-} # "search_tags" и "searchstr" c запроса в network
-
-response = requests.post(url, json=payload)
-data = response.json()
-
-lots = data['aData']['lots']['data'] # записываю нужные данные из всего ответа
+}
 
 with open("practice1.csv", "w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(["id", "name_ru"])
 
-    for lot in lots:
-        writer.writerow([
-            lot.get("id"),
-            lot.get("name_ru")
-        ])  # записываю id и name_ru для файла
+    for page in range(1, last_page + 1):
+        url = f"{base_url}{page}"
+        response = requests.post(url, json=payload)
+        data = response.json()
 
-print("Ответ с id и name_ru записан в файл practice1.csv")
+        lots = data['aData']['lots']['data']
+
+        for lot in lots:
+            writer.writerow([
+                lot.get("id"),
+                lot.get("name_ru")
+            ])
+
+print("All lots with id and name_ru are saved in the practice1.csv file")
