22bf52dc3dfaa898c2a561b07b9a77685cde1675
Dasha-K8 <karlova.dsh@gmail.com>
2025-10-03 16:02:33 +0300

    new commit

--- practice.py
+++ practice.py
@@ -1,10 +1,10 @@
 import requests
 import csv
 
-base_url = "https://api.tenderbot.kz/api/lots?page="
-last_page = 15
-
+base_url = "https://api.tenderbot.kz/api/lots"
 keyword = "компьютер"
+page = 1
+
 payload = {
     "search_tags": [{"text": keyword, "tiClasses": ["ti-valid"]}],
     "searchstr": keyword
@@ -14,9 +14,9 @@ with open("practice1.csv", "w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(["id", "name_ru"])
 
-    for page in range(1, last_page + 1):
-        url = f"{base_url}{page}"
-        response = requests.post(url, json=payload)
+    while True:
+        params = {"page": page}
+        response = requests.post(base_url, params=params, json=payload)
         data = response.json()
 
         lots = data['aData']['lots']['data']
@@ -27,4 +27,14 @@ with open("practice1.csv", "w", newline="", encoding="utf-8") as file:
                 lot.get("name_ru")
             ])
 
+        next_page_url = data['aData']['lots'].get('next_page_url')
+
+        if next_page_url is None:
+            break
+        page += 1
+
 print("All lots with id and name_ru are saved in the practice1.csv file")
+
+
+
+
