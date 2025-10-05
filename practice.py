506b4892162cc179c1285f9f19acfd59e9af8d6f
Dasha-K8 <karlova.dsh@gmail.com>
2025-10-05 10:52:05 +0300

    new commit

--- practice.py
+++ practice.py
@@ -5,7 +5,6 @@ base_url = "https://api.tenderbot.kz/api/lots"
 keyword = "компьютер"
 page = 1
 
-
 with open("practice1.csv", "w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(["id", "name_ru"])
@@ -13,9 +12,48 @@ with open("practice1.csv", "w", newline="", encoding="utf-8") as file:
     while True:
 
         payload = {
-            "search_tags": [{"text": keyword, "tiClasses": ["ti-valid"]}],
+            "methods": [],
+            "tags": [],
+            "search_tags": [
+                {
+                    "text": keyword,
+                    "tiClasses": [
+                        "ti-valid"
+                    ]
+                }
+            ],
+            "rate_id": 1,
+            "rate_name": "KZT",
+            "subject": [],
+            "sort_col": "lots.created_at",
+            "sort_type": "asc",
+            "excluded": [],
+            "partners": [],
             "searchstr": keyword,
+            "types": [],
+            "summ": [],
+            "send_lots": "",
+            "send_mail": "",
+            "today": [],
+            "hasitogt2": 0,
+            "exactMatch": 0,
+            "publicate": [],
+            "closed": [],
+            "search_type": [],
+            "export_xls": "",
+            "link": "lots.list",
+            "bot_id": "",
+            "bot_name": "",
+            "max_amount": "",
+            "markPko": 0,
+            "markTph": 0,
+            "markInvalid": 0,
+            "markMedicine": 0,
+            "cstatus": [],
+            "customers": [],
+            "kato_id": [],
             "page": page
+
         }
         response = requests.post(base_url, json=payload)
         data = response.json()
@@ -35,6 +73,41 @@ with open("practice1.csv", "w", newline="", encoding="utf-8") as file:
         page += 1
 
 print("All lots with id and name_ru are saved in the practice1.csv file")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
