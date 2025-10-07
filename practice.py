af4492c623a595ac72f1bb8a0d581995dbe35cd0
Dasha-K8 <karlova.dsh@gmail.com>
2025-10-07 11:13:53 +0300

    Запись данных в бд монго

--- practice.py
+++ practice.py
@@ -1,118 +1,72 @@
 import requests
-import csv
+from pymongo import MongoClient
+import os
+import time
+
+
+time.sleep(5)
+mongo_uri = os.getenv("MONGO_URI", "mongodb://mongo:27017/purchases")
+client = MongoClient(mongo_uri)
+db = client.purchases
+collection = db.lots
 
 base_url = "https://api.tenderbot.kz/api/lots"
 keyword = "компьютер"
 page = 1
 
-with open("practice1.csv", "w", newline="", encoding="utf-8") as file:
-    writer = csv.writer(file)
-    writer.writerow(["id", "name_ru"])
-
-    while True:
-
-        payload = {
-            "methods": [],
-            "tags": [],
-            "search_tags": [
-                {
-                    "text": keyword,
-                    "tiClasses": [
-                        "ti-valid"
-                    ]
-                }
-            ],
-            "rate_id": 1,
-            "rate_name": "KZT",
-            "subject": [],
-            "sort_col": "lots.created_at",
-            "sort_type": "asc",
-            "excluded": [],
-            "partners": [],
-            "searchstr": keyword,
-            "types": [],
-            "summ": [],
-            "send_lots": "",
-            "send_mail": "",
-            "today": [],
-            "hasitogt2": 0,
-            "exactMatch": 0,
-            "publicate": [],
-            "closed": [],
-            "search_type": [],
-            "export_xls": "",
-            "link": "lots.list",
-            "bot_id": "",
-            "bot_name": "",
-            "max_amount": "",
-            "markPko": 0,
-            "markTph": 0,
-            "markInvalid": 0,
-            "markMedicine": 0,
-            "cstatus": [],
-            "customers": [],
-            "kato_id": [],
-            "page": page
-
+payload = {
+    "methods": [],
+    "tags": [],
+    "search_tags": [
+        {
+            "text": keyword,
+            "tiClasses": [
+                "ti-valid"
+            ]
         }
-        response = requests.post(base_url, json=payload)
-        data = response.json()
+    ],
+    "rate_id": 1,
+    "rate_name": "KZT",
+    "subject": [],
+    "sort_col": "lots.created_at",
+    "sort_type": "asc",
+    "excluded": [],
+    "partners": [],
+    "searchstr": keyword,
+    "types": [],
+    "summ": [],
+    "send_lots": "",
+    "send_mail": "",
+    "today": [],
+    "hasitogt2": 0,
+    "exactMatch": 0,
+    "publicate": [],
+    "closed": [],
+    "search_type": [],
+    "export_xls": "",
+    "link": "lots.list",
+    "bot_id": "",
+    "bot_name": "",
+    "max_amount": "",
+    "markPko": 0,
+    "markTph": 0,
+    "markInvalid": 0,
+    "markMedicine": 0,
+    "cstatus": [],
+    "customers": [],
+    "kato_id": []
 
-        lots = data['aData']['lots']['data']
+}
+response = requests.post(base_url, json=payload)
+data = response.json()
 
-        for lot in lots:
-            writer.writerow([
-                lot.get("id"),
-                lot.get("name_ru")
-            ])
+lots = data['aData']['lots']['data']
 
-        next_page_url = data['aData']['lots'].get('next_page_url')
+for lot in lots:
+    document = {
+        "id": lot.get("id"),
+        "name_ru": lot.get("name_ru")
+    }
+    collection.insert_one(document)
 
-        if next_page_url is None:
-            break
-        page += 1
-
-print("All lots with id and name_ru are saved in the practice1.csv file")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("All lots have been recorded in the MongoDB database.")
